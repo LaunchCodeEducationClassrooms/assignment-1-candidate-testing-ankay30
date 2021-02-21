ee5e7059b311e7d81df1d019846e2d71c5b14406
ankayarkanni muthuramasubramanian <ankay30@gmail.com>
2021-02-21 01:00:55 +0000

    Declaration updated

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,14 +6,14 @@ const input = require('readline-sync');
 let candidateName ='';
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who is the first American women in space? ";
+let question = "Who was the first American women in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer ='';
-let questions = ['1.Who was the first American woman in space:  '     , '2.True or False:5000 meters = 5 kilometers : ' ,
-    '3.(5+3)/2*10 = ? :  ',
-    '4.Given the array [8, "Orbit" , "Trajectory" , 45], what entry is at index 2? :  ',
-    '5.What is the minimum crew size for the ISS? : '];
-let correctAnswers = ["Sally Ride", "True","40" ,"Trajectory" ,"3"] ;
+let questions = ['Who was the first American woman in space? '     , 'True or false: 5 kilometer == 5000 meters? ' ,
+    '(5 + 3)/2 * 10 = ? ',
+    'Given the array [8, "Orbit" , "Trajectory" , 45], what entry is at index 2? ',
+    'What is the minimum crew size for the ISS? '];
+let correctAnswers = ["Sally Ride", "true","40" ,"Trajectory" ,"3"] ;
 let candidateAnswers = [];
 //console.log(correctAnswers);
 
